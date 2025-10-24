da00d9677f1aee9ffa475a03731b579be565a350
soumsa55 <39264251+soumsa55@users.noreply.github.com>
2025-10-24 13:10:16 +0530

    Add welcome message for Soumyaranjan Sahu

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ package com.example;
 public class MyApp {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins and Maven!");
+        System.out.println("Hello Soumyaranjan Sahu and welcome to the world of Dev Ops!");
     }
 }
