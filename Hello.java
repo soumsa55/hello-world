ff0bfa09cd3cd7843589e82032673a2f71be5b7f
soumsa55 <39264251+soumsa55@users.noreply.github.com>
2025-10-24 13:24:47 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ public class MyApp {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins and Maven!");
         System.out.println("Hello Soumyaranjan Sahu and welcome to the world of Dev Ops!");
+         System.out.println("Checking the webhook!");
     }
 }
